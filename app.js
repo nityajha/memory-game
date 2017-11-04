4ba5573970421b64ddeae260f6b8f3f52fc24581
nityajha <31059295+nityajha@users.noreply.github.com>
2017-11-04 20:25:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,24 +25,24 @@ function shuffle(array) {
     return array;
 }
 
-var array = ["#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8"];
+var card_array = ["#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8"];
 var clicked_cards = [];
 var counter = 0;
 var moves = 0;
 var clicked_card_id = 0;
 var firstId = 0;
 var secondId = 0;
+var score = 0;
 
 function clicked(this){
 	$('.card').click(function (e) {
 	  $(this).toggleClass('show');
 	});
-	store_id(this);
 	moves = moves + 1;
 }
 
-function store_id(this){
-	first_id = ;
+function store_id(){
+	first_id = $(this).attr("id") ;
 }
 
 function store_in_array(first_id){
@@ -55,21 +55,57 @@ function check(){
 	if (clicked_cards[0] == clicked_cards[1]){
 		clicked_cards[0].addClass('match');
 		clicked_cards[1].addClass('match');
+		score = score + 5;
 		pairs = pairs + 1;
 		check_win();
 	}else{
-		clicked_cards[0].removeClass('show');
-		clicked_cards[1].removeClass('show');
 		clicked_cards.pop();
 		clicked_cards.pop();
+		score = score - 1;
 	}
 }
 
-check_win(pairs){
+function check_win(pairs){
 	if (pairs === 8){
-		declare winner;
+		alert("You have won the game with" +moves+" moves. Press restart symbol to play again.");
 	}
 }
+function reset_game(game){
+	pairs = 0;
+	clicked_cards = [];
+	moves = 0;
+	clicked_card_id = 0;
+	score = 0;
+	firstId = 0;
+	secondId = 0;
+	shuffle(card_array);
+};
+	
+
+/*function check(){
+	for (counter = 0; counter < 2; counter++){
+		counter = counter + 1;
+		if(counter == 1){
+			checkMatch();
+			firstId = getElementById;
+		}
+		if(counter == 2){
+			checkMatch();
+			secondId = getElementById;
+		}
+		if (firstId == secondId){
+			$(firstId).addClass('match');
+			$(secondId).addClass('match');
+		}
+		else{
+			counter = 0;
+			firstId = 0;
+			secondId = 0;
+		}
+	}
+}*/
+
+//https://calendly.com/career-coach/careers-jp/
 
 /*
  * set up the event listener for a card. If a card is clicked:
