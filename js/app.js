773cbbf01cff763401fa5342276daf98150e88fc
nityajha <31059295+nityajha@users.noreply.github.com>
2017-12-03 12:59:35 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ function store_in_array($clickedCard){
 
 function store_card_id($clickedCard){
 	for(var i = 0; i<2; i++){
-		const cardid = clicked_cards[i].children().attr("id");
+		var cardid = clicked_cards[i].children().attr("id");
 		clicked_card_id.push(cardid);
 	}
 	//https://stackoverflow.com/questions/18867599/jquery-inarray-how-to-use-it-right
